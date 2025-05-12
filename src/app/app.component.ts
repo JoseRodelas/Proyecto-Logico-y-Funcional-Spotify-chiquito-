22e41548b7dfa3b44900bfa06fd78702f5ab20e1
KevinPompa <21610631@itcdcuauhtemoc.edu.mx>
2025-05-12 09:20:23 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Proyecto-Logico-y-Funcional-Spotify-chiquito-';
+  nombreUsuario: any;
+  theme: string = 'light-theme';
+  usuarioLogueado = false;
+
+  constructor(private sesionService: SesionService, private router: Router, private renderer: Renderer2) {}
+
+  ngOnInit(): void  {
+    this.sesionService.usuario$.subscribe(usuario => {
+      this.usuarioLogueado = !!usuario;
+    });
+
+    // Verificar si hay un tema guardado en el localStorage
+    const savedTheme = localStorage.getItem('theme') || 'light-theme';
+    this.setTheme(savedTheme);
+
+    // Suscribirse al servicio de sesión para obtener el usuario actual
+    this.sesionService.usuario$.subscribe(usuario => {
+      this.nombreUsuario = usuario;
+    });
+  }
+
+  toggleTheme(): void {
+    const newTheme = this.theme === 'light-theme' ? 'dark-theme' : 'light-theme';
+    this.setTheme(newTheme);
+  }
+
+  private setTheme(theme: string): void {
+    this.theme = theme;
+    localStorage.setItem('theme', theme);
+
+    const body = document.body;
+    this.renderer.removeClass(body, 'light-theme');
+    this.renderer.removeClass(body, 'dark-theme');
+    this.renderer.addClass(body, theme);
+  }
+
+  cerrarSesion() {
+    this.sesionService.cerrarSesion();
+    this.router.navigate(['/login']);
+    this.nombreUsuario = null;
+  }
 }

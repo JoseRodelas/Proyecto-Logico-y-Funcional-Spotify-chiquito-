042c14172836777b8b516932af4940c2bbdb6487
José Rodelas <josealexisrodelas41@gmail.com>
2025-05-15 14:45:05 -0600

    Control de reproduccion y servicios de Spotify

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  spotifyClientId: 'TU_CLIENT_ID_PROD',
-  redirectUri: 'https://tuapp.azurewebsites.net/callback',
-  scopes: 'user-read-private user-read-email'
-};

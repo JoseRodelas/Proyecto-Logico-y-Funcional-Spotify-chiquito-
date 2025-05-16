d94b721e6f677351deb2c8ff4b8a5b26cda4dec1
hamp <gelidicers98@gmail.com>
2025-05-15 20:02:20 -0600

    Busqueda por categoria
    Añadi todo respecto a la busqueda por categoria de cancion, es escroleable pero se puede expandit para que te deje ver mas canciones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  currentCategory: string = 'pop';
+  showAllSongs: boolean = false;
+  displayAllSongs: Song[] = [];
 
+  ngOnInit(): void {
+    this.loadSongs();
+  }
+  
+  selectCategory(categoryId: string): void {
+    this.currentCategory = categoryId;
+    this.showAllSongs = false;
+    this.loadSongs();
+  }
+
+  loadSongs(): void {
+    this.displayedSongs = this.showAllSongs ? [...this.songs[this.currentCategory]] : this.songs[this.currentCategory].slice(0, 4);
+  }
+
+  toggleShowAll(): void {
+    this.showAllSongs = !this.showAllSongs;
+    this.loadSongs();
+  }
+
+  getCategoryName(): string {
+    return this.categories.find(c => c.id === this.currentCategory)?.name || 'Musica';
+  }
+
+  playSong(song: Song): void {
+    console.log(`Reproduciendo: ${song.title}`);
+  }
+  
 }

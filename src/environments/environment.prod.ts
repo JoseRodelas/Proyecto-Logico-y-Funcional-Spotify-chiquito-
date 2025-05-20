82315fbcd8c64547a1d45b5dee3ce9dc72a843c1
kevin <1111111111kmr@gmail.com>
2025-05-20 09:15:40 -0600

    Pagina de favoritos
    Pagina de Favoritos agregada

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  spotifyClientId: 'TU_CLIENT_ID_PROD',
+  redirectUri: 'https://tuapp.azurewebsites.net/callback',
+  scopes: 'user-read-private user-read-email'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  spotifyClientId: '883b452674924c99b93ae99633df61fc',
+  redirectUri: 'http://localhost:4200/callback',
+  scopes: 'user-library-read user-top-read user-read-email user-read-private user-read-playback-position user-modify-playback-state user-read-playback-state user-read-currently-playing streaming'
+};

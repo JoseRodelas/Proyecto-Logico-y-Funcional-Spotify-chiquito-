503e7f2a97a410fe97adf1224e1c3200dee23d8c
kevin <1111111111kmr@gmail.com>
2025-05-20 09:23:40 -0600

    Revert "Pagina de favoritos"
    This reverts commit 82315fbcd8c64547a1d45b5dee3ce9dc72a843c1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { SesionService } from './servicios/sesion.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,45 +6,5 @@ import { SesionService } from './servicios/sesion.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombreUsuario: any;
-  theme: string = 'light-theme';
-  usuarioLogueado = false;
-
-  constructor(private sesionService: SesionService, private router: Router, private renderer: Renderer2) {}
-
-  ngOnInit(): void  {
-    this.sesionService.usuario$.subscribe(usuario => {
-      this.usuarioLogueado = !!usuario;
-    });
-
-    // Verificar si hay un tema guardado en el localStorage
-    const savedTheme = localStorage.getItem('theme') || 'light-theme';
-    this.setTheme(savedTheme);
-
-    // Suscribirse al servicio de sesión para obtener el usuario actual
-    this.sesionService.usuario$.subscribe(usuario => {
-      this.nombreUsuario = usuario;
-    });
-  }
-
-  toggleTheme(): void {
-    const newTheme = this.theme === 'light-theme' ? 'dark-theme' : 'light-theme';
-    this.setTheme(newTheme);
-  }
-
-  private setTheme(theme: string): void {
-    this.theme = theme;
-    localStorage.setItem('theme', theme);
-
-    const body = document.body;
-    this.renderer.removeClass(body, 'light-theme');
-    this.renderer.removeClass(body, 'dark-theme');
-    this.renderer.addClass(body, theme);
-  }
-
-  cerrarSesion() {
-    this.sesionService.cerrarSesion();
-    this.router.navigate(['/login']);
-    this.nombreUsuario = null;
-  }
+  title = 'Proyecto-Logico-y-Funcional-Spotify-chiquito-';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,19 @@ import { AppComponent } from './app.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { LoginComponent } from './components/login/login.component';
-
-import { FormsModule } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service'; 
-
-import { HttpClientModule } from '@angular/common/http';
-import { CallbackComponent } from './pages/callback/callback.component';
-import { PlayerComponent } from './pages/spotify-player/spotify-player.component';
-import { FavoritesComponent } from './components/favorites/favorites.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    HomeComponent,
-    LoginComponent,
-    CallbackComponent,
-    PlayerComponent,
-    FavoritesComponent,
+    AboutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [CookieService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
